52199717e3be2f5fcedbd3632e3be0e9b3c9c76c
Harikrishna583 <41606382+Harikrishna583@users.noreply.github.com>
2025-11-23 19:30:21 +0530

    Update db.js
    this is db.js file host variables updated

--- db.js
+++ db.js
@@ -1,26 +1,29 @@
 const mysql = require('mysql2/promise');
 require('dotenv').config();
 
-// const pool = mysql.createPool({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-//   waitForConnections: true,
-//   connectionLimit: 10,
-// });
-
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "fitness_platform",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   timezone: '+05:30', // Set to IST
   dateStrings: true, // Return dates as strings
   charset: 'utf8mb4'
 });
+
+// const pool = mysql.createPool({
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "fitness_platform",
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   timezone: '+05:30', // Set to IST
+//   dateStrings: true, // Return dates as strings
+//   charset: 'utf8mb4'
+// });
 
 
 
